45c3ccc5df71a7292f43ad8e1aceb3a475ba7741
kunalsgodara <kunalsgodara@gmail.com>
2024-03-20 01:37:49 +0530

    model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,7 +16,7 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRFRegressor
 
-from src.exception import CustomException
+from src.exceptions import Custom_exception
 from src.logger import logging
 
 from src.utils import save_object,evaluate_models
@@ -44,7 +44,39 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "XGBRegressor": XGBRegressor(),
+                "XGBRegressor": XGBRFRegressor,
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+
+            ## To get best Model
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get the best Model name 
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+
+            ## if no best model found
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            
+            logging.info(f"Best model found on both trainnig and testing dataset")
+
+
+            save_object(file_path=self.model_trainer_config.trained_model_file_path,obj=best_model)
+
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test,predicted)
+
+            return r2_square
+            
+        except Exception as e:
+            raise Custom_exception(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 # import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -21,3 +22,30 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise Custom_exception(e, sys)
+    
+
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train,y_train) #train model
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+            return report
+        
+    except Exception as e:
+        raise Custom_exception(e,sys)
+        
+    
